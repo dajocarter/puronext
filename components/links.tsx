88cc05d959dba32f3b661d634eab09931397f3aa
dajocarter <dajocarter@gmail.com>
2022-09-05 13:17:10 -0700

    replace StyledButtonLink with scss

--- components/links.tsx
+++ components/links.tsx
@@ -1,47 +1,6 @@
 import React, { useState, useEffect, ReactElement, Children } from 'react'
 import { useRouter } from 'next/router'
-import styled from 'styled-components'
 import Link, { LinkProps } from 'next/link'
-
-export const StyledButtonLink = styled.a<{
-  primary?: boolean
-  secondary?: boolean
-}>`
-  border-width: 3px;
-  border-style: solid;
-  border-color: ${(props) =>
-    props.primary
-      ? props.theme.primary
-      : props.secondary
-      ? props.theme.secondary
-      : props.theme.alt};
-  background-color: transparent;
-  color: black;
-  display: inline-block;
-  font-size: 16px;
-  letter-spacing: 1px;
-  line-height: 1.5;
-  padding: 0.5rem 1.5rem;
-  text-align: center;
-  text-decoration: none;
-  text-transform: uppercase;
-  transition-property: background-color, border-color, color;
-  transition-duration: 0.15s;
-  transition-timing-function: cubic-bezier(0.785, 0.135, 0.15, 0.86);
-
-  &:hover,
-  &:focus {
-    background-color: ${(props) =>
-      props.primary
-        ? props.theme.primary
-        : props.secondary
-        ? props.theme.secondary
-        : props.theme.alt};
-    color: white;
-    cursor: pointer;
-    text-decoration: none;
-  }
-`
 
 type ActiveLinkProps = LinkProps & {
   children: ReactElement
