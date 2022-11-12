f02371cc08a9b15c83eea57a866b831b78123d89
David Carter <dajocarter@gmail.com>
2022-11-12 15:10:08 -0800

    add latin subset to google fonts

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['wordpress.puroflux.com']
+  },
+  experimental: {
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    ],
   }
 }
 
